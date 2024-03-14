ed51e157bac7edbb97c390bc73519ba2042aaad9
Johannes Kresner <johannes.kresner@gmail.com>
2024-03-14 15:12:35 +0100

    fix: updated oil config

--- lua/jokresner/lazy/oil.lua
+++ lua/jokresner/lazy/oil.lua
@@ -2,7 +2,24 @@ return {
   "stevearc/oil.nvim",
   config = function()
     local oil = require("oil")
-    oil.setup()
+    oil.setup({
+      default_file_explorer = true,
+      columns = {
+        "icon",
+        "size",
+      },
+      win_options = {
+        signcolumn = 'yes',
+        spell = true,
+        list = true,
+      },
+      delete_to_trash = true,
+      promt_save_on_select_new_entry = true,
+      experimental_watch_for_changes = true,
+      view_options = {
+        show_hidden = true,
+      },
+    })
 
     vim.keymap.set("n", "-", oil.open, { desc = "Open parent directory" })
   end,
