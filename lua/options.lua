ee6bc26f1219e89b4936b3a3b947833726f4c0ce
Johannes Kresner <johannes@jokresner.de>
2025-03-09 12:06:13 +0100

    feat: add minimap

--- lua/options.lua
+++ lua/options.lua
@@ -8,6 +8,22 @@ vim.g.have_nerd_font = true
 -- Make relative line numbers default
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.diagnostic.config({ -- color line number with hint color
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.INFO] = "",
+			[vim.diagnostic.severity.HINT] = "",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
+			[vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+			[vim.diagnostic.severity.HINT] = "DiagnosticHint",
+		},
+	},
+})
 
 -- Enable mouse mode
 vim.opt.mouse = "a"
