94e199f99696cf5161aa711d423c80764c46fa81
Johannes Kresner <johannes@jokresner.de>
2025-03-27 22:45:24 +0100

    fix: default blink config

--- lua/plugins/blink-cpm.lua
+++ lua/plugins/blink-cpm.lua
@@ -1,146 +1,49 @@
 return {
 	"saghen/blink.cmp",
-	version = not vim.g.lazyvim_blink_main and "*",
-	build = vim.g.lazyvim_blink_main and "cargo build --release",
-	opts_extend = {
-		"sources.completion.enabled_providers",
-		"sources.compat",
-		"sources.default",
-	},
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-		-- add blink.compat to dependencies
-		{
-			"saghen/blink.compat",
-			optional = true, -- make optional so it's only enabled if any extras need it
-			opts = {},
-			version = not vim.g.lazyvim_blink_main and "*",
-		},
-	},
-	event = "InsertEnter",
+	dependencies = { "rafamadriz/friendly-snippets" },
+
+	version = "1.*",
 
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
-		snippets = {
-			expand = function(snippet, _)
-				return LazyVim.cmp.expand(snippet)
-			end,
-		},
+		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+		-- 'super-tab' for mappings similar to vscode (tab to accept)
+		-- 'enter' for enter to accept
+		-- 'none' for no mappings
+		--
+		-- All presets have the following mappings:
+		-- C-space: Open menu or open docs if already open
+		-- C-n/C-p or Up/Down: Select next/previous item
+		-- C-e: Hide menu
+		-- C-k: Toggle signature help (if signature.enabled = true)
+		--
+		-- See :h blink-cmp-config-keymap for defining your own keymap
+		keymap = { preset = "default" },
+
 		appearance = {
-			-- sets the fallback highlight groups to nvim-cmp's highlight groups
-			-- useful for when your theme doesn't support blink.cmp
-			-- will be removed in a future release, assuming themes add support
-			use_nvim_cmp_as_default = false,
-			-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- adjusts spacing to ensure icons are aligned
+			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
 			nerd_font_variant = "mono",
 		},
-		completion = {
-			accept = {
-				-- experimental auto-brackets support
-				auto_brackets = {
-					enabled = true,
-				},
-			},
-			menu = {
-				draw = {
-					treesitter = { "lsp" },
-				},
-			},
-			documentation = {
-				auto_show = true,
-				auto_show_delay_ms = 200,
-			},
-			ghost_text = {
-				enabled = vim.g.ai_cmp,
-			},
-		},
 
-		-- experimental signature help support
-		-- signature = { enabled = true },
+		-- (Default) Only show the documentation popup when manually triggered
+		completion = { documentation = { auto_show = true } },
 
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			-- adding any nvim-cmp sources here will enable them
-			-- with blink.compat
-			compat = {},
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
 
-		cmdline = {
-			enabled = false,
-		},
-
-		keymap = {
-			preset = "enter",
-			["<C-y>"] = { "select_and_accept" },
-		},
+		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		--
+		-- See the fuzzy documentation for more information
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
-	---@param opts blink.cmp.Config | { sources: { compat: string[] } }
-	config = function(_, opts)
-		-- setup compat sources
-		local enabled = opts.sources.default
-		for _, source in ipairs(opts.sources.compat or {}) do
-			opts.sources.providers[source] = vim.tbl_deep_extend(
-				"force",
-				{ name = source, module = "blink.compat.source" },
-				opts.sources.providers[source] or {}
-			)
-			if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
-				table.insert(enabled, source)
-			end
-		end
-
-		-- add ai_accept to <Tab> key
-		if not opts.keymap["<Tab>"] then
-			if opts.keymap.preset == "super-tab" then -- super-tab
-				opts.keymap["<Tab>"] = {
-					require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
-					LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-					"fallback",
-				}
-			else -- other presets
-				opts.keymap["<Tab>"] = {
-					LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-					"fallback",
-				}
-			end
-		end
-
-		-- Unset custom prop to pass blink.cmp validation
-		opts.sources.compat = nil
-
-		-- check if we need to override symbol kinds
-		for _, provider in pairs(opts.sources.providers or {}) do
-			---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
-			if provider.kind then
-				local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-				local kind_idx = #CompletionItemKind + 1
-
-				CompletionItemKind[kind_idx] = provider.kind
-				---@diagnostic disable-next-line: no-unknown
-				CompletionItemKind[provider.kind] = kind_idx
-
-				---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
-				local transform_items = provider.transform_items
-				---@param ctx blink.cmp.Context
-				---@param items blink.cmp.CompletionItem[]
-				provider.transform_items = function(ctx, items)
-					items = transform_items and transform_items(ctx, items) or items
-					for _, item in ipairs(items) do
-						item.kind = kind_idx or item.kind
-						item.kind_icon = LazyVim.config.icons.kinds[item.kind_name] or item.kind_icon or nil
-					end
-					return items
-				end
-
-				-- Unset custom prop to pass blink.cmp validation
-				provider.kind = nil
-			end
-		end
-
-		require("blink.cmp").setup(opts)
-	end,
+	opts_extend = { "sources.default" },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
