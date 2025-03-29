01e63520277d694ee8ae844e8d5876dbfe0e64c1
Johannes Kresner <johannes@jokresner.de>
2025-03-29 21:26:05 +0100

    fix: - formatting - adding trouble

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,26 +1,26 @@
 return {
-	"folke/which-key.nvim",
-	event = "VimEnter", -- Sets the loading event to 'VimEnter'
-	opts = {
-		icons = {
-			mappings = vim.g.have_nerd_font,
-		},
-		spec = {
+  "folke/which-key.nvim",
+  event = "VimEnter", -- Sets the loading event to 'VimEnter'
+  opts = {
+    icons = {
+      mappings = vim.g.have_nerd_font,
+    },
+    spec = {
       { "<leader>a", group = "[A]vante AI" },
-			{ "<leader>b", group = "[B]uffer" },
-			{ "<leader>c", group = "[C]ode", mode = { "n", "x" } },
-			{ "<leader>d", group = "[D]ocument" },
+      { "<leader>b", group = "[B]uffer" },
+      { "<leader>c", group = "[C]ode", mode = { "n", "x" } },
+      { "<leader>d", group = "[D]ocument" },
       { "<leader>f", group = "[F]ind" },
-			{ "<leader>g", group = "[G]it" },
-			{ "<leader>r", group = "[R]eference" },
-			{ "<leader>s", group = "[S]earch" },
-			{ "<leader>u", group = "+Toggle" },
-			{ "<leader>w", group = "[W]orkspace" },
-			{ "<leader>t", group = "[T]oggle" },
-			{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
-			{ "<leader>x", group = "+Trouble" },
-		},
-	},
+      { "<leader>g", group = "[G]it" },
+      { "<leader>r", group = "[R]eference" },
+      { "<leader>s", group = "[S]earch" },
+      { "<leader>u", group = "+Toggle" },
+      { "<leader>w", group = "[W]orkspace" },
+      { "<leader>t", group = "[T]rouble" },
+      { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+      { "<leader>x", group = "+Trouble" },
+    },
+  },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -11,35 +11,18 @@ set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute the current
 
 -- Toggle hlsearch if it's on, otherwise just do "enter"
 set("n", "<CR>", function()
-	---@diagnostic disable-next-line: undefined-filed
-	if vim.opt.hlsearch:get() then
-		vim.cmd.nohl()
-		return ""
-	else
-		return "<CR>"
-	end
+  ---@diagnostic disable-next-line: undefined-field
+  if vim.opt.hlsearch:get() then
+    vim.cmd.nohl()
+    return ""
+  else
+    return "<CR>"
+  end
 end, { expr = true })
 
 set("n", "<space>tt", function()
   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = 0 }, { bufnr = 0 })
 end)
 
-set("n", "j", function(...)
-  local count = vim.v.count
-
-  if count == 0 then
-    return "gj"
-  else
-    return "j"
-  end
-end, { expr = true })
-
-set("n", "k", function(...)
-  local count = vim.v.count
-
-  if count == 0 then
-    return "gk"
-  else
-    return "k"
-  end
-end, { expr = true })
+set("n", "<C-u>", "<C-u>zz", { silent = true })
+set("n", "<C-d>", "<C-d>zz", { silent = true })
