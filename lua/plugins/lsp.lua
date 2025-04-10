9f28cedf1e15654e80ee190772c614940773d506
Johannes Kresner <johannes@jokresner.de>
2025-04-10 22:57:36 +0200

    feat: add copilot - quickfix lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -96,47 +96,47 @@ return {
         lua = true,
       }
 
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local bufnr = args.buf
-          local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
-
-          local settings = servers[client.name]
-          if type(settings) ~= "table" then
-            settings = {}
-          end
-
-          local builtin = require "telescope.builtin"
-
-          vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
-          vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0, desc = "Goto Definition" })
-          vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0, desc = "Goto Reference" })
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0, desc = "Goto Declaration" })
-          vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0, desc = "Goto Type Definition" })
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0, desc = "Hover Documentation" })
-
-          vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0, desc = "Code Rename" })
-          vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0, desc = "Code Action" })
-          vim.keymap.set("n", "<space>wd", builtin.lsp_document_symbols, { buffer = 0, "Symbols in current document" })
-
-          local filetype = vim.bo[bufnr].filetype
-          if disable_semantic_tokens[filetype] then
-            client.server_capabilities.semanticTokensProvider = nil
-          end
-
-          -- Override server capabilities
-          if settings.server_capabilities then
-            for k, v in pairs(settings.server_capabilities) do
-              if v == vim.NIL then
-                ---@diagnostic disable-next-line: cast-local-type
-                v = nil
-              end
-
-              client.server_capabilities[k] = v
-            end
-          end
-        end,
-      })
+      -- vim.api.nvim_create_autocmd("LspAttach", {
+      --   callback = function(args)
+      --     local bufnr = args.buf
+      --     local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+      --
+      --     local settings = servers[client.name]
+      --     if type(settings) ~= "table" then
+      --       settings = {}
+      --     end
+      --
+      --     local builtin = require "telescope.builtin"
+      --
+      --     vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+      --     vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0, desc = "Goto Definition" })
+      --     vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0, desc = "Goto Reference" })
+      --     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0, desc = "Goto Declaration" })
+      --     vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0, desc = "Goto Type Definition" })
+      --     vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0, desc = "Hover Documentation" })
+      --
+      --     vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0, desc = "Code Rename" })
+      --     vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0, desc = "Code Action" })
+      --     vim.keymap.set("n", "<space>wd", builtin.lsp_document_symbols, { buffer = 0, "Symbols in current document" })
+      --
+      --     local filetype = vim.bo[bufnr].filetype
+      --     if disable_semantic_tokens[filetype] then
+      --       client.server_capabilities.semanticTokensProvider = nil
+      --     end
+      --
+      --     -- Override server capabilities
+      --     if settings.server_capabilities then
+      --       for k, v in pairs(settings.server_capabilities) do
+      --         if v == vim.NIL then
+      --           ---@diagnostic disable-next-line: cast-local-type
+      --           v = nil
+      --         end
+      --
+      --         client.server_capabilities[k] = v
+      --       end
+      --     end
+      --   end,
+      -- })
 
       require("config.autoformat").setup()
 
