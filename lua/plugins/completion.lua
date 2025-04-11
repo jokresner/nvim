bc818c9c3bb3ee16b8a28c0fb3fb62065502e2b3
Johannes Kresner <johannes@jokresner.de>
2025-04-11 10:03:12 +0200

    feat: use blink for copilot completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,7 +1,10 @@
 return {
   "saghen/blink.cmp",
   -- optional: provides snippets for the snippet source
-  dependencies = { "rafamadriz/friendly-snippets" },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    "giuxtaposition/blink-cmp-copilot",
+  },
 
   -- use a release tag to download pre-built binaries
   version = "1.*",
@@ -49,7 +52,15 @@ return {
       },
     },
     sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
+      default = { "lsp", "path", "snippets", "buffer", "copilot" },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
+        },
+      },
     },
     fuzzy = { implementation = "prefer_rust_with_warning" },
     cmdline = { enabled = false },
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -10,7 +10,7 @@ return {
         auto_trigger = true,
         hide_during_completion = vim.g.ai_cmp,
         keymap = {
-          accept = false,
+          accept = false, -- handled by blink.cmp
           next = "<M-]>",
           prev = "<M-[>",
         },
