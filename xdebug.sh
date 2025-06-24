f57f1ecd2363dffcb7d6ec614cdc4d84902baba1
Johannes Kresner <johannes@jokresner.de>
2025-06-24 09:51:26 +0200

    feat: xdebug config

---
+++ xdebug.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ELECTRON_RUN_AS_NODE=1 "/Applications/Cursor.app/Contents/Frameworks/Cursor Helper (Plugin).app/Contents/MacOS/Cursor Helper (Plugin)" "$HOME/.local/share/nvim/mason/packages/php-debug-adapter/extension/out/phpDebug.js"
