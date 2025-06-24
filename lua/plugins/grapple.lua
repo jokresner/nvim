a6211205ea17b40746364bccc6f0717cd2165a1c
Johannes Kresner <johannes@jokresner.de>
2025-06-24 23:37:44 +0200

    fix: grapple keymap

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -5,7 +5,7 @@ return {
     local set = vim.keymap.set
     local grapple = require "grapple"
 
-    set("n", "<leader>h", grapple.toggle, { desc = "Toggle Grapple" })
-    set("n", "<leader>H", grapple.toggle_tags, { desc = "Toggle Grapple Tags" })
+    set("n", "<leader>y", grapple.toggle, { desc = "Toggle Grapple" })
+    set("n", "<leader>Y", grapple.toggle_tags, { desc = "Toggle Grapple Tags" })
   end,
 }
