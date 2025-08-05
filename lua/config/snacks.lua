d01a39762111df915a7f1d4b1d185dd2b46c8147
Johannes Kresner <johannes@jokresner.de>
2025-08-05 23:35:39 +0200

    feat: working ast-grep

--- lua/config/snacks.lua
+++ lua/config/snacks.lua
@@ -4,71 +4,52 @@ local M = {}
 
 local uv = vim.uv or vim.loop
 
-local function get_cmd(opts, filter)
-  local pattern = Snacks.picker.util.parse(filter.search)
-
-  local cmd = "ast-grep"
-  local args = {
-    "-p",
-    pattern,
-    "--json",
-  }
-
-  local paths = {}
-
-  if opts.buffers then
-    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-      local name = vim.api.nvim_buf_get_name(buf)
-      if name ~= "" and vim.bo[buf].buflistged and uv.fs_stat(name) then
-        paths[#paths + 1] = name
+function M.ast_grep_picker()
+  Snacks.picker.pick {
+    format = "file",
+    notify = false, -- Also prevents error when searching with additional arguments
+    show_empty = true,
+    live = true,
+    supports_live = true,
+    -- hidden = true,
+    -- ignored = true,
+    ---@param opts snacks.picker.grep.Config
+    finder = function(opts, ctx)
+      local cmd = "ast-grep"
+      local args = { "run", "--color=never", "--json=stream" }
+      if vim.fn.has "win32" == 1 then
+        cmd = "sg"
       end
-    end
-  end
-
-  vim.list_extend(paths, opts.dirs or {})
-  if opts.rtp then
-    vim.list_extend(paths, Snacks.picker.util.rtp())
-  end
-
-  if #paths > 0 then
-    paths = vim.tbl_map(svim.fs.normalize, paths)
-    vim.list_extend(args, paths)
-  end
-
-  return cmd, args
-end
-
-function M.ast_grep_picker(opts, ctx)
-  if opts.need_search ~= false and ctx.filter.search == "" then
-    return function() end
-  end
-
-  local absolute = (opts.dirs and #opts.dirs > 0) or opts.buffers or opts.rtp
-  local cwd = not absolute and svim.fs.normalize(opts and opts.cwd or uv.cwd() or ".") or nil
-  local cmd, args = get_cmd(opts, ctx.filter)
-
-  return require("snacks.picker.source.proc").proc({
-    opts,
-    {
-      notify = false,
-      cmd = cmd,
-      args = args,
-      transform = function(item)
-        item.cwd = cwd
-        local file, line, col, text = item.text:match "^(.+):(%d+):(%d+):(.*)$"
-        if not file then
-          if not item.text:match "WARNING" then
-            snacks.notify.error("invalid ast-grep output:\n" .. item.text)
-          end
-          return false
-        else
-          item.line = text
-          item.file = file
-          item.pos = { tonumber(line), tonumber(col) - 1 }
-        end
-      end,
-    },
-  }, ctx)
+      if opts.hidden then
+        table.insert(args, "--no-ignore=hidden")
+      end
+      if opts.ignored then
+        table.insert(args, "--no-ignore=vcs")
+      end
+      local pattern, pargs = Snacks.picker.util.parse(ctx.filter.search)
+      table.insert(args, string.format("--pattern=%s", pattern))
+      vim.list_extend(args, pargs)
+      return require("snacks.picker.source.proc").proc({
+        opts,
+        {
+          cmd = cmd,
+          args = args,
+          transform = function(item)
+            local entry = vim.json.decode(item.text)
+            if vim.tbl_isempty(entry) then
+              return false
+            else
+              local start = entry.range.start
+              item.cwd = svim.fs.normalize(opts and opts.cwd or vim.uv.cwd() or ".") or nil
+              item.file = entry.file
+              item.line = entry.text
+              item.pos = { tonumber(start.line) + 1, tonumber(start.column) }
+            end
+          end,
+        },
+      }, ctx)
+    end,
+  }
 end
 
 return M
