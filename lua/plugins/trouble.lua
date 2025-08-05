f6a1bd20617040db4c67a5dbb50dd07e90c568cd
Johannes Kresner <johannes@jokresner.de>
2025-08-06 00:07:18 +0200

    chore: floatingtodo

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,24 +1,36 @@
 return {
   "folke/trouble.nvim",
   cond = vim.g.vscode == nil,
-  specs = {
-    "folke/snacks.nvim",
-    opts = function(_, opts)
-      return vim.tbl_deep_extend("force", opts or {}, {
-        picker = {
-          actions = require("trouble.sources.snacks").actions,
-          win = {
-            input = {
-              keys = {
-                ["<c-t>"] = {
-                  "trouble_open",
-                  mode = { "n", "i" },
-                },
-              },
-            },
-          },
-        },
-      })
-    end,
+  keys = {
+    {
+      "<leader>td",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>tD",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>tl",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>tq",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
   },
 }
