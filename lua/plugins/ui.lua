966ac623592cb7d650243805fb50975c650e4880
Johannes Kresner <johannes@jokresner.de>
2025-09-13 12:57:51 +0200

    feat: add lensline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -174,4 +174,12 @@ return {
       require("pretty-fold").setup()
     end,
   },
+  {
+    "oribarilan/lensline.nvim",
+    tag = "1.1.2", -- or: branch = 'release/1.x' for latest non-breaking updates
+    event = "LspAttach",
+    config = function()
+      require("lensline").setup()
+    end,
+  },
 }
