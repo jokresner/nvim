aabeee989da2303accbf40ce80e4900fb28b08a0
Johannes Kresner <johannes@jokresner.de>
2025-10-08 12:25:39 +0200

    fix: snippy config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -122,7 +122,7 @@ return {
       vim.api.nvim_set_keymap("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
       vim.api.nvim_set_keymap("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
       vim.api.nvim_set_keymap("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
-      vim.api.nvim_set_keymap("n", "<Leader>l", "<Cmd>noh<CR>", kopts)
+      vim.api.nvim_set_keymap("n", "<leader>lu", "<Cmd>noh<CR>", kopts)
     end,
   },
   {
