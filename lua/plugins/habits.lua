4f5ffdb418fb8a54d1a12d1f3f063fe24d8db7b6
Johannes Kresner <johannes@jokresner.de>
2025-11-19 22:56:49 +0100

    feat: add hardtime and precognition

---
+++ lua/plugins/habits.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "m4xshen/hardtime.nvim",
+    cond = vim.g.vscode == nil,
+    event = "VeryLazy",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {
+      enabled = false,
+    },
+  },
+  {
+    "tris203/precognition.nvim",
+    cond = vim.g.vscode == nil,
+    event = "VeryLazy",
+    opts = {
+      startVisible = true,
+      showBlankVirtLine = true,
+      highlightColor = { link = "Comment" },
+      hints = {
+        Caret = { text = "^", prio = 2 },
+        Dollar = { text = "$", prio = 1 },
+        MatchingPair = { text = "%", prio = 5 },
+        Zero = { text = "0", prio = 1 },
+        w = { text = "w", prio = 10 },
+        b = { text = "b", prio = 9 },
+        e = { text = "e", prio = 8 },
+        W = { text = "W", prio = 7 },
+        B = { text = "B", prio = 6 },
+        E = { text = "E", prio = 5 },
+      },
+      gutterHints = {
+        G = { text = "G", prio = 10 },
+        gg = { text = "gg", prio = 9 },
+        PrevParagraph = { text = "{", prio = 8 },
+        NextParagraph = { text = "}", prio = 8 },
+      },
+    },
+  },
+}
