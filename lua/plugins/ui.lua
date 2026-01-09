c8f968eff0c43316039581e3e41e095f7bb6f594
Johannes Kresner <johannes@jokresner.de>
2026-01-09 01:45:47 +0100

    feat: replace catppuccin colorscheme with koda

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -9,7 +9,16 @@ return {
       return require("config.ui").catppuccin
     end,
     config = function()
-      vim.cmd.colorscheme "catppuccin"
+      --vim.cmd.colorscheme "catppuccin"
+    end,
+  },
+  {
+    "oskarnurm/koda.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("koda").setup { transparent = true }
+      vim.cmd "colorscheme koda"
     end,
   },
   {
